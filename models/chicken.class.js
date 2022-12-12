deb343f6ce64b57f42502ac5c123bd3f58dd6b59
Martin Dalic <Martin.Dalic@gmail.com>
2022-12-12 04:46:12 +0100

    scale objects

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,7 +1,7 @@
 class Chicken extends MovableObject {
-    y = 390;
-  width = 40;
-  height = 40;
+    y = 360;
+  width = 70;
+  height = 70;
   constructor() {
     super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     this.x = 200 + Math.random() * 500;

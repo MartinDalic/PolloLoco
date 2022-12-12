de744f4312cb287a880bf7e1f04d030201541a38
Martin Dalic <Martin.Dalic@gmail.com>
2022-12-12 04:10:33 +0100

    BagroundObject

---
+++ models/background-object.class.js
@@ -0,0 +1,10 @@
+class BagroundObject extends MovableObject {
+    width= 720;
+    height=400;
+    constructor(imagePath,x,y) {
+        super().loadImage(imagePath);
+        this.x = x;
+        this.y = y;
+
+    }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -1,7 +1,10 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
-  canvas;
+  clouds = [new Cloud()];
+  bagroundObjects = [
+    new BagroundObject("img/5_background/layers/3_third_layer/1.png",0,120),
+  ];
   ctx;
 
   constructor(canvas) {
@@ -11,28 +14,29 @@ class World {
   }
 
   draw() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.drawImage(
-      this.character.img,
-      this.character.x,
-      this.character.y,
-      this.character.width,
-      this.character.height
-    );
 
-    this.enemies.forEach((enemy) => {
-      this.ctx.drawImage(
-        enemy.img,
-        enemy.x,
-        enemy.y,
-        enemy.width,
-        enemy.height
-      );
-    });
+      
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.addToMap(this.character);
+      
+      this.addObjectsToMap(this.enemies);
+      this.addObjectsToMap(this.clouds);
+      this.addObjectsToMap(this.bagroundObjects);
+
 
     let self = this;
     requestAnimationFrame(function () {
       self.draw();
     });
+  }
+
+  addObjectsToMap(objects) {
+    objects.forEach((object) => {
+      this.addToMap(object);
+    });
+  }
+  
+  addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
   }
 }

4bfd107826bcc93115202dd8737c8ab1333f00d5
Martin Dalic <Martin.Dalic@gmail.com>
2022-12-12 11:07:50 +0100

    add more elements in bagroundObjects

--- models/world.class.js
+++ models/world.class.js
@@ -3,10 +3,30 @@ class World {
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   clouds = [new Cloud()];
   bagroundObjects = [
-    new BagroundObject("img/5_background/layers/air.png", 0, 120),
-    new BagroundObject("img/5_background/layers/3_third_layer/1.png", 0, 120),
-    new BagroundObject("img/5_background/layers/2_second_layer/1.png", 0, 120),
-    new BagroundObject("img/5_background/layers/1_first_layer/1.png", 0, 120),
+    new BagroundObject("img/5_background/layers/air.png", 719*-1),
+    new BagroundObject("img/5_background/layers/3_third_layer/2.png", 719*-1),
+    new BagroundObject("img/5_background/layers/2_second_layer/2.png", 719*-1),
+    new BagroundObject("img/5_background/layers/1_first_layer/2.png", 719*-1),
+
+    new BagroundObject("img/5_background/layers/air.png", 719*0),
+    new BagroundObject("img/5_background/layers/3_third_layer/1.png", 719*0),
+    new BagroundObject("img/5_background/layers/2_second_layer/1.png", 719*0),
+    new BagroundObject("img/5_background/layers/1_first_layer/1.png", 719*0),
+
+    new BagroundObject("img/5_background/layers/air.png", 719*1),
+    new BagroundObject("img/5_background/layers/3_third_layer/2.png", 719*1),
+    new BagroundObject("img/5_background/layers/2_second_layer/2.png", 719*1),
+    new BagroundObject("img/5_background/layers/1_first_layer/2.png", 719*1),
+
+    new BagroundObject("img/5_background/layers/air.png", 719*2),
+    new BagroundObject("img/5_background/layers/3_third_layer/1.png", 719*2),
+    new BagroundObject("img/5_background/layers/2_second_layer/1.png", 719*2),
+    new BagroundObject("img/5_background/layers/1_first_layer/1.png", 719*2),
+
+    new BagroundObject("img/5_background/layers/air.png", 719*3),
+    new BagroundObject("img/5_background/layers/3_third_layer/2.png", 719*3),
+    new BagroundObject("img/5_background/layers/2_second_layer/2.png", 719*3),
+    new BagroundObject("img/5_background/layers/1_first_layer/2.png", 719*3),
   ];
   canvas;
   ctx;

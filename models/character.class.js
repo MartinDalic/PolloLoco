fc866646e97ccfdd3308b7a2d5b2ecaa77acc076
Martin Dalic <Martin.Dalic@gmail.com>
2022-12-12 07:32:38 +0100

    Pepe move right and move back

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,7 @@
 class Character extends MovableObject {
     y = 150;
     height = 280;
+    speed = 10;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -11,6 +12,7 @@ class Character extends MovableObject {
     ]
     currentImage = 0;
     chrWorld;
+
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
@@ -23,14 +25,26 @@ class Character extends MovableObject {
         setInterval(() => {
 
             if(this.chrWorld.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+            if(this.chrWorld.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+
+        }, 1000 / 60);
+
+        setInterval(() => {
+
+            if(this.chrWorld.keyboard.RIGHT) {
+
+                // Walk Animation
                 let i = this.currentImage % this.IMAGES_WALKING.length; // remainder (%) operator
                 let path = this.IMAGES_WALKING[i]
                 this.img = this.imageCache[path];
                 this.currentImage++;
-
             }
 
-        }, 100);
+        }, 50);
     }
 
 
